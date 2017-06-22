212d04f1f707e73a1743cb060f71269eda371c52
zhu xun <zhuxun@zhudeMacBook-Pro.local>
2017-06-22 09:23:03 +0800

    修正swoole启动；

--- trunk/start.sh
+++ trunk/start.sh
@@ -9,6 +9,8 @@ composer install
 cd /data/wwwroot/trunk/bin
 ps -fe|grep \[s]tart_swoole_server
 if [[ $? -ne 0 ]]; then
+    echo "start swoole..."
+    rm -f /data/wwwroot/trunk/src/*.php.pid
     php start_swoole_server.php start
 else
     echo "start_swoole_server is running..."
